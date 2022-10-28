e716d52f595cad28ece2c1e12b4c6af706346efd
6379721812 <115489152+6379721812@users.noreply.github.com>
2022-10-28 13:43:22 +0530

    Delete Assingnments/Team Member 3(Sakthivel K)/Assignment 2 directory

--- Assingnments/Team Member 3(Sakthivel K)/Assignment 2/db.py
+++
@@ -1,4 +0,0 @@
-import sqlite3
-db = sqlite3.connect('users.db')
-db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT NOT NULL,password TEXT NOT NULL,name TEXT NOT NULL,email TEXT NOT NULL)");
-db.commit()
